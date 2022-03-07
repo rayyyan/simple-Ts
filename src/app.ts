8fa850281d1163b0690368eb2b857a0b1859d6a1
ibenchi <horizen27@gmail.com>
2022-03-07 22:51:14 +0000

    handling DOM

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+const form = document.querySelector("form")! as HTMLFormElement
+const addressInput = document.getElementById("address")! as HTMLInputElement
+function searchAddressHandler(event: Event) {
+  event.preventDefault()
+  const enteredAddress = addressInput.value
+
+  //sending to google api
+}
+
+form.addEventListener("submit", searchAddressHandler)
