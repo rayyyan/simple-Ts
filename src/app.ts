8a8d433fbc38b92ea3d5ab39cbf44b167ac84149
ibenchi <horizen27@gmail.com>
2022-03-08 09:17:38 +0000

    show google map with a marker

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,38 @@
+import axios from "axios"
+
 const form = document.querySelector("form")! as HTMLFormElement
 const addressInput = document.getElementById("address")! as HTMLInputElement
 
-console.log(process.env.DB_HOST)
-
+const Key = process.env.API_KEY
+type GoogleGeoCodingResponse = {
+  result: { geometry: { location: { lat: number; lng: number } } }[]
+  status: "OK" | "ZERO_RESULTS"
+}
+declare var google: any
 function searchAddressHandler(event: Event) {
   event.preventDefault()
   const enteredAddress = addressInput.value
-
+  axios
+    .get<GoogleGeoCodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${Key}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location ")
+      }
+      const coordinates = response.data.result[0].geometry.location
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 17,
+      })
+      new google.maps.Marker({ position: coordinates, map: map })
+    })
+    .catch((err) => {
+      alert(err.message)
+      console.log(err)
+    })
   //sending to google api
 }
 
